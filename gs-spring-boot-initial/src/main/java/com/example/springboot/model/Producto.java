62e0bf996a85f99e02e5201c48ba5d9db6efd78a
Alejandro Di Stefano <addistefano@live.com.ar>
2024-01-29 13:49:54 -0300

    Mejoras en las Cards

--- gs-spring-boot-initial/src/main/java/com/example/springboot/model/Producto.java
+++ gs-spring-boot-initial/src/main/java/com/example/springboot/model/Producto.java
@@ -9,46 +9,74 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Producto {
-	 public Producto() {
-	    }
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nombre;
-    public Producto(Long id, String nombre, String descripcion, String imagen) {
-		super();
-		this.id = id;
-		this.nombre = nombre;
-		this.descripcion = descripcion;
-		this.imagen = imagen;
-	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getDescripcion() {
-		return descripcion;
-	}
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
-	}
-	public String getImagen() {
-		return imagen;
-	}
-	public void setImagen(String imagen) {
-		this.imagen = imagen;
-	}
-	private String descripcion;
+    private String descripcion;
     private String imagen;
+    private Double price;
+    private Integer stock;
 
-    
+    public Producto() {
+    }
+
+    public Producto(Long id, String nombre, String descripcion, String imagen, Double price, Integer stock) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
 }
